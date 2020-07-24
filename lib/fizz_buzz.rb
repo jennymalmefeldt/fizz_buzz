7aa05ba4c7ad1a503e08b7d74f64204b039e0c13
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-07-24 14:05:34 +0200

    done with fizz buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,13 +1,16 @@
 
 def fizz_buzz(number)
     
-    if number % 3 == 0
-      'fizz'
-    elsif
-      number % 5 == 0
-      'buzz'
-    else
-      number
+    if number % 15 == 0
+      'fizzbuzz'
 
-      end
+    elsif number % 5 == 0
+      'buzz'
+
+    elsif number % 3 == 0
+      'fizz'
+
+    else
+        number
+  end
 end 
